3e872514df13057929eae91b7f723f9b5994c83c
Rasujon3 <rasujon3@gmail.com>
2022-06-04 23:59:16 +0600

    fetch data using redux toolkit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import CounterView from "./features/counter/CounterView";
+import PostView from "./features/posts/PostView";
 
 function App() {
   return (
     <div className="App">
       <h1>Counter App</h1>
       <CounterView />
+      <PostView />
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
+import postReducer from "../features/posts/postSlice";
 import counterReducer from "./../features/counter/counterSlice";
 
 const store = configureStore({
   reducer: {
     counter: counterReducer,
+    posts: postReducer,
   },
 });
 
